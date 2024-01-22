d204e89ff2254730dbf2ba753f5adf9ea279b03c
Acauan Nascimento <acauan.nascimento@hotmail.com(none)>
2024-01-22 00:12:12 -0300

    feat: Carrossel de imagens/Media Queries

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,26 +1,63 @@
-// const initProductSlider = () => {
-//   const productList = document.querySelector(".product-list");
-//   const productSlideButtons = document.querySelectorAll(".product-slider-btn");
+/* ------------------------------CAROUSEL------------------------------ */
+const carouselButtons = document.querySelectorAll(".button");
+const images = document.querySelectorAll(".image");
 
-//   const maxScrollLeft = productList.scrollWidth - productList.clientWidth;
+carouselButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    disableSelectedButton();
 
-//   productSlideButtons.forEach((button) => {
-//     button.addEventListener("click", () => {
-//       const direction = button.id === "prev-slide" ? -1 : 1;
-//       const scrollAmount = productList.clientWidth * direction;
-//       productList.scrollBy({ left: scrollAmount, behavior: "smooth" });
-//     });
-//   });
-//   const handleProductSlideButtons = () => {
-//     productSlideButtons[0].style.display =
-//       productList.scrollLeft <= 0 ? "none" : "block";
-//     productSlideButtons[1].style.display =
-//       productList.scrollLeft >= maxScrollLeft ? "none" : "block";
-//   };
+    selectCarouselButton(button);
 
-//   productList.addEventListener("scroll", () => {
-//     handleProductSlideButtons();
-//   });
-// };
+    hideActiveImage();
 
-// window.addEventListener("load", initProductSlider);
+    showBackgroundImage(index);
+  });
+});
+
+function showBackgroundImage(index) {
+  images[index].classList.add("active");
+}
+
+function selectCarouselButton(button) {
+  button.classList.add("selected");
+}
+
+function hideActiveImage() {
+  const activeImage = document.querySelector(".active");
+  activeImage.classList.remove("active");
+}
+
+function disableSelectedButton() {
+  const selectedButton = document.querySelector(".selected");
+  selectedButton.classList.remove("selected");
+}
+/* ------------------------------CAROUSEL------------------------------ */
+
+/* ------------------------------PRODUCT SLIDER------------------------------ */
+const initProductSlider = () => {
+  const productList = document.querySelector(".product-list");
+  const productSlideButtons = document.querySelectorAll(".product-slider-btn");
+
+  const maxScrollLeft = productList.scrollWidth - productList.clientWidth;
+
+  productSlideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === "prev-slide" ? -1 : 1;
+      const scrollAmount = productList.clientWidth * direction;
+      productList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
+  });
+  const handleProductSlideButtons = () => {
+    productSlideButtons[0].style.display =
+      productList.scrollLeft <= 0 ? "none" : "block";
+    productSlideButtons[1].style.display =
+      productList.scrollLeft >= maxScrollLeft ? "none" : "block";
+  };
+
+  productList.addEventListener("scroll", () => {
+    handleProductSlideButtons();
+  });
+};
+
+window.addEventListener("load", initProductSlider);
+/* ------------------------------PRODUCT SLIDER------------------------------ */
